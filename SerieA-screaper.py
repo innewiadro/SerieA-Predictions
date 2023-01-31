81a04fcd5d9703195b722a724a089cc439a3dc0c
Michal <innewiadro@gmail.com>
2023-01-31 18:40:11 +0100

    -Scraping data for multiple season and teams with a loop

--- SerieA-screaper.py
+++ SerieA-screaper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 
 standings_url = "https://fbref.com/en/comps/11/2021-2022/2021-2022-Serie-A-Stats"
@@ -10,6 +11,7 @@ data.text
 # print(data.text)
 
 soup = BeautifulSoup(data.text, features="html.parser")
+print(soup)
 standings_table = soup.select('table.stats_table')[0]
 links = standings_table.find_all('a')
 links = [l.get("href") for l in links]
@@ -50,3 +52,50 @@ team_data = matches[0].merge(shooting[["Date", "Sh", "SoT", "Dist", "FK", "PK", 
 # print(shooting.shape)
 
 """Scraping data for multiple season and teams with a loop"""
+
+years = list(range(2021, 2019, -1))
+
+all_matches = []
+
+standings_url = "https://fbref.com/en/comps/11/2021-2022/2021-2022-Serie-A-Stats"
+
+for year in years:
+    data = requests.get(standings_url)
+    soup = BeautifulSoup(data.text, features="html.parser")
+    standings_table = soup.select('table.stats_table')[0]
+
+    links = [l.get("href") for l in standings_table.find_all('a')]
+    links = [l for l in links if "/squads" in l]
+    team_urls = [f'https://fbref.com{l}' for l in links]
+
+    previous_season = soup.select("a.prev")[0].get("href")
+    standings_url = f'https://fbref.com/{previous_season}'
+
+    for team_url in team_urls:
+        team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+
+        data = requests.get(team_url)
+        matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
+
+        soup = BeautifulSoup(data.text, features="html.parser")
+        links = [l.get("href") for l in soup.find_all('a')]
+        links = [l for l in links if l and "all_comps/shooting/" in l]
+        data = requests.get(f"https://fbref.com{links[0]}")
+        shooting = pd.read_html(data.text, match="Shooting")[0]
+        shooting.columns = shooting.columns.droplevel()
+
+        try:
+            team_data = matches.merge(shooting[["Date", "Sh", "SoT", "Dist", "FK", "PK", "PKatt"]], on="Date")
+        except ValueError:
+            continue
+
+        team_data = team_data[team_data["Comp"] == "Serie A"]
+        team_data["Season"] = year
+        team_data["Team"] = team_name
+        all_matches.append(team_data)
+        time.sleep(2)
+
+match_df = pd.concat(all_matches)
+match_df.columns = [c.lower() for c in match_df.columns]
+match_df.to_csv("matches_data.csv")
+
